e996c03043f246953dc43d9ad5c923320b5da650
D-Tr3x <owosenidaniel13@gmail.com>
2024-09-15 16:33:06 +0100

    Pacifying checker

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for alpha in range(ord('a'), ord('z') +1):
-    if alpha == ord('q') or alpha == ord('e'):
-        print("", end="")
-    else:
+for alpha in range(ord('a'), ord('z') + 1):
+    if alpha != ord('q') and alpha != ord('e'):
         print("{}".format(chr(alpha)), end="")
