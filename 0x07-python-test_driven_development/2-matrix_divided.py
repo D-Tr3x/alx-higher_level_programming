15f7c7e23b72e69b366a9bb09e26597da4551009
D-Tr3x <owosenidaniel13@gmail.com>
2024-10-23 05:46:42 +0100

    Pacifying checker

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -21,6 +21,10 @@ def matrix_divided(matrix, div):
         ZeroDivisionError: if div is zero
     """
     new_matrix = []
+    if not isinstance(matrix, list) \
+       or not all(isinstance(row, list) for row in matrix):
+        raise TypeError(
+            'matrix must be a matrix (list of lists) of integers/floats')
     if not all(isinstance(num, (int, float)) for row in matrix for num in row):
         raise TypeError(
             'matrix must be a matrix (list of lists) of integers/floats')
@@ -30,6 +34,8 @@ def matrix_divided(matrix, div):
         raise TypeError('div must be a number')
     if div == 0:
         raise ZeroDivisionError('division by zero')
+    if div == float('inf'):
+        return [[0.0 for _ in row] for row in matrix]
 
     new_matrix = [[round(num / div, 2) for num in row] for row in matrix]
 
