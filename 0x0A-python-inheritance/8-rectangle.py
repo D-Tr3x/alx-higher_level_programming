a8d1ced691b2bed2c4e36e5b940fce3baff9a6cc
D-Tr3x <owosenidaniel13@gmail.com>
2024-10-29 19:39:11 +0100

    Pacifying checker for 8...

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-""" This module provides geometry classes. """
+""" This module provides geometry classes.
+"""
 
 
 class BaseGeometry:
     """ Base class for geometric shapes. """
 
     def area(self):
-        raise Exception('area() is not implemented')
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         if not isinstance(value, int):
             raise TypeError('{} must be an integer'.format(name))
+        if isinstance(value, bool):
+            raise TypeError('{} must be an integer'.format(name))
         if value <= 0:
-            raise ValueError('{} must be greater than 0'. format(name))
-
+            raise ValueError('{} must be greater than 0'.format(name))
 
 class Rectangle(BaseGeometry):
     """ Rectangle class that inherits from BaseGeometry class """
