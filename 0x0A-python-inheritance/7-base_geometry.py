5bd4da58302fb8a738f6b63254ed57aa5e09fdf5
D-Tr3x <owosenidaniel13@gmail.com>
2024-10-29 19:13:35 +0100

    Pacifying Checker

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -10,7 +10,9 @@ class BaseGeometry:
         raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
-        if not isinstance(value, int) or isinstance(value, bool):
+        if not isinstance(value, int):
+            raise TypeError('{} must be an integer'.format(name))
+        if isinstance(value, bool):
             raise TypeError('{} must be an integer'.format(name))
         if value <= 0:
             raise ValueError('{} must be greater than 0'.format(name))
