0b0532ad5fbd227e2dd074ea35f876db79e26ccd
D-Tr3x <owosenidaniel13@gmail.com>
2024-11-23 19:46:15 +0100

    Updating 2-create_read_user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,9 +4,8 @@ CREATE DATABASE
     IF NOT EXISTS hbtn_0d_2;
 CREATE USER
     IF NOT EXISTS 'user_0d_2'@'localhost'
-    IDENTIFIED BY 'user_0d_2_pwd';
+    IDENTIFIED WITH mysql_native_password BY 'user_0d_2_pwd';
 GRANT SELECT
-    ON *.*
-    TO 'user_0d_2'@'localhost'
-    WITH GRANT OPTION;
+    ON hbtn_0d_2.*
+    TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
