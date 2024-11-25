a57f3e98bf4e585f003b6b050263cc20bbe167c3
D-Tr3x <owosenidaniel13@gmail.com>
2024-11-25 04:36:17 +0100

    ALL UPPERCASED

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- Lists all shows, and all genres linked to that show
-SELECT tv_shows.title AS title, tv_genres.name as name
+SELECT tv_shows.title AS title, tv_genres.name AS name
   FROM tv_shows
        LEFT JOIN tv_show_genres
        ON tv_shows.id = tv_show_genres.show_id
