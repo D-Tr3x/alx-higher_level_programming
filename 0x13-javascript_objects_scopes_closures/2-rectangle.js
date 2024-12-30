cdb7f968192d0745cfd341a091b867e973e80c3a
D-Tr3x <owosenidaniel13@gmail.com>
2024-12-30 15:40:29 +0100

    Corrected errors

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,13 +1,9 @@
 #!/usr/bin/node
 
 class Rectangle {
-  width;
-  height;
-
   constructor (w, h) {
     if (!Number.isInteger(w) || !Number.isInteger(h) || w <= 0 || h <= 0) {
-      this.width = undefined;
-      this.height = undefined;
+      return;
     } else {
       this.width = w;
       this.height = h;
