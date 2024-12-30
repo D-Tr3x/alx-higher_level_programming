19b01ef96b07e2563871a1d960a04433dfdee816
D-Tr3x <owosenidaniel13@gmail.com>
2024-12-30 15:44:34 +0100

    Semestandard validation

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,9 +2,7 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (!Number.isInteger(w) || !Number.isInteger(h) || w <= 0 || h <= 0) {
-      return;
-    } else {
+    if (Number.isInteger(w) && Number.isInteger(h) && w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
