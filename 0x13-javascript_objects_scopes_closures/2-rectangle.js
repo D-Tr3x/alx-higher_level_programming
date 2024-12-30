fc8f36af6731259e13d0aeed696da77c9b154d8c
D-Tr3x <owosenidaniel13@gmail.com>
2024-12-30 15:35:06 +0100

    Pacifying checker

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -6,9 +6,8 @@ class Rectangle {
 
   constructor (w, h) {
     if (!Number.isInteger(w) || !Number.isInteger(h) || w <= 0 || h <= 0) {
-      // this.width = undefined;
-      // this.height = undefined;
-      return {};
+      this.width = undefined;
+      this.height = undefined;
     } else {
       this.width = w;
       this.height = h;
