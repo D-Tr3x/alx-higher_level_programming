6b1c606d3e553b34473fac11bbcf551a9c2c3e1b
D-Tr3x <owosenidaniel13@gmail.com>
2024-12-30 15:33:31 +0100

    Write a class Rectangle that defines a rectangle based on 1-rectangle: creating an empty object if w or h is invalid

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+class Rectangle {
+  width;
+  height;
+
+  constructor (w, h) {
+    if (!Number.isInteger(w) || !Number.isInteger(h) || w <= 0 || h <= 0) {
+      // this.width = undefined;
+      // this.height = undefined;
+      return {};
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+module.exports = Rectangle;
--- 0x13-javascript_objects_scopes_closures/README.md
+++ 0x13-javascript_objects_scopes_closures/README.md
@@ -1,2 +1,3 @@
 0. Rectangle #0
 1. Rectangle #1
+2. Rectangle #2
